1b34d8bda790b06c5bdee0d1c278bdaf77befff6
KatalinaNadasi <katalina.nadasi@gmail.com>
2017-02-28 14:48:19 +0100

    footer arrow javascript ok

--- js/script.js
+++ js/script.js
@@ -16,17 +16,42 @@ $(function() {
       $('.footer-arrow').fadeIn(1000);
     }
   });
+  $(window).scroll(function() {
+  if ($(document).height() <= ($(window).height() + $(window).scrollTop()))
+  {
+    $('.footer-arrow').css({
+      position: 'fixed',
+      bottom: 150
+      });
+    } else {
+    $('.footer-arrow').css({
+      position: 'fixed',
+      bottom: 20
+    });
+  }
+  var doc = $(document);
+
+  doc.scroll(function () {
+      // make sure to wrap yours entire footer in some css selector
+      var footer = $('#wholeFooter');
+      var arrow = $('.footer-arrow');
+      var s = $('#product-summary-position');
+
+      var top = doc.scrollTop() + s.offset().top * 2 + p.height();
+      var footerTop = footer.offset().top;
+
+      var offset = footerTop - top;
+
+      if (offset < 0) {
+          p.css({'margin-top': '' + offset + 'px'});
+      } else {
+          p.css({'margin-top': 0});
+      }
+    });
+  })
 });
 
 
-
-// $(window).scroll(function() {
-//     if ($(this).scrollTop() > 200) { //use `this`, not `document`
-//         $('.fixedelement').css({
-//             'display': 'none'
-//         });
-//     }
-// });
 
 // hover tentative
 // $('.product-content').mouseover(function()
